837b0661bf98460b1f7d25143ec2e928ed32bd2e
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-21 13:43:45 +0200

    ex021-custom-hooks-useLocalStorage

---
+++ origamid/exercicios/ex021-custom-hooks-useLocalStorage/aula.jsx
@@ -0,0 +1,37 @@
+// - Custom Hooks
+
+// useLocalStorage
+// Podemos criar nossos próprios hooks, assim evitamos a repetição de código. Todo custom hook deve começar com a palavra use. Exemplo: useNomeDoHook. Podemos retornar o que quisermos do hook, seja um valor único, uma array ou um objeto.
+
+//useLocalStorage.jsx
+const useLocalStorage = (key, inicial) => {
+  const [state, setState] = React.useState(() => {
+    const local = window.localStorage.getItem(key);
+    return local ? local : inicial;
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [key, state]);
+
+  return [state, setState];
+};
+
+//App.js
+import useLocalStorage from "./useLocalStorage";
+
+const App = () => {
+  const [produto, setProduto] = useLocalStorage("produto", "");
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
+  return (
+    <div>
+      <p>Preferido: {produto}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  );
+};
