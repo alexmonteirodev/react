b486dd7b73f4f10aa78f543bbe6cba5748974bcc
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-21 14:20:41 +0200

    ex022-custom-hooks-useFetch

---
+++ origamid/exercicios/ex022-custom-hooks-useFetch/aula.jsx
@@ -0,0 +1,54 @@
+// - custom hook
+// - useFetch
+// Aqui o useCallback é necessário para evitar um render infinito.
+
+//useFetch.jsx
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, loading, error, request };
+};
+
+export default useFetch;
+
+//App.js
+import React from 'react';
+import useFetch from './useFetch';
+
+const App = () => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    request('https://ranekapi.origamid.dev/json/api/produto/notebook');
+  }, [request]);
+
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data) return <div>{data.nome}</div>;
+  else return null;
+};
+
+export default App;
