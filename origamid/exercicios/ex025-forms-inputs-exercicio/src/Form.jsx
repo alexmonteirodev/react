7f85b26f54e6647d58937da5a0384c1fe83990bc
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-21 16:25:03 +0200

    ex025-forms-inputs-exercicio - resolvento part1

---
+++ origamid/exercicios/ex025-forms-inputs-exercicio/src/Form.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import "./form.css";
+
+const Form = () => {
+  const [nome, setNome] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [senha, setSenha] = React.useState("");
+  const [cep, setCep] = React.useState("");
+  const [rua, setRua] = React.useState("");
+  const [numero, setNumero] = React.useState("");
+  const [bairro, setBairro] = React.useState("");
+  const [cidade, setCidade] = React.useState("");
+  const [estado, setEstado] = React.useState("");
+
+  return (
+    <form action="">
+      <label htmlFor="nome">Nome:</label>
+      <input
+        value={nome}
+        type="text"
+        id="nome"
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <p>{nome}</p>
+
+      <label htmlFor="email">Email:</label>
+      <input
+        value={email}
+        type="email"
+        id="email"
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <p>{email}</p>
+
+      <label htmlFor="senha">Senha:</label>
+      <input
+        value={senha}
+        type="password"
+        id="senha"
+        onChange={(event) => setSenha(event.target.value)}
+      />
+      <p>{senha}</p>
+
+      <label htmlFor="cep">cep:</label>
+      <input
+        value={cep}
+        type="text"
+        id="cep"
+        onChange={(event) => setCep(event.target.value)}
+      />
+      <p>{cep}</p>
+
+      <label htmlFor="rua">rua:</label>
+      <input
+        value={rua}
+        type="text"
+        id="rua"
+        onChange={(event) => setRua(event.target.value)}
+      />
+      <p>{rua}</p>
+
+      <label htmlFor="numero">numero:</label>
+      <input
+        value={numero}
+        type="number"
+        id="numero"
+        onChange={(event) => setNumero(event.target.value)}
+      />
+      <p>{numero}</p>
+
+      <label htmlFor="bairro">bairro:</label>
+      <input
+        value={bairro}
+        type="text"
+        id="bairro"
+        onChange={(event) => setBairro(event.target.value)}
+      />
+      <p>{bairro}</p>
+
+      <label htmlFor="cidade">cidade:</label>
+      <input
+        value={cidade}
+        type="text"
+        id="cidade"
+        onChange={(event) => setCidade(event.target.value)}
+      />
+      <p>{cidade}</p>
+
+      <label htmlFor="estado">estado:</label>
+      <input
+        value={estado}
+        type="text"
+        id="estado"
+        onChange={(event) => setEstado(event.target.value)}
+      />
+      <p>{estado}</p>
+
+      <button>Enviar</button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ origamid/exercicios/ex025-forms-inputs-exercicio/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Form from "./Form.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Form />
+  </React.StrictMode>
+);
