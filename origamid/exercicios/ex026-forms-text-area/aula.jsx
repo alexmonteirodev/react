c27174fdf5adec2d630087960b601bb313b57d50
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-22 11:23:38 +0200

    ex026-forms-text-area

---
+++ origamid/exercicios/ex026-forms-text-area/aula.jsx
@@ -0,0 +1,17 @@
+// - Textarea
+// No React o textarea é utilizado como um input, uma tag única sem abertura/fechamento e com o value para definir o seu valor interno.
+const App = () => {
+  const [mensagem, setMensagem] = React.useState("");
+
+  return (
+    <form>
+      <textarea
+        id="mensagem"
+        value={mensagem}
+        rows="5" //escolhe quantidade de linhas
+        onChange={({ target }) => setMensagem(target.value)}
+      />
+      <p>{mensagem}</p>
+    </form>
+  );
+};
