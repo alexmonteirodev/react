029ea1bdab51055e39786f1193445f8c1b6ca059
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-04-23 17:11:41 +0200

    ex036-forms-desafio - solucionando

---
+++ origamid/exercicios/ex036-forms-desafio/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
+
+//questões para resolver.
+
+// 1 - da pra dar proxima sem responder
+
+// 2 - mostrar resultado no fim
